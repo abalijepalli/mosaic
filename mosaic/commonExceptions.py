4d6bad383a2589421d7e49d4d8a54152536811d3
abalijepalli <arvind.balijepalli@nist.gov>
2016-05-25 19:48:54 -0400

    Update links.

--- mosaic/commonExceptions.py
+++
@@ -1,9 +0,0 @@
-"""
-	Common exception types used across the system.
-"""
-class SettingsTypeError(Exception):
-	def __init__(self, value):
-		self.value="Setting {0} has an invalid type.".format(str(value).split()[-1])
-
-	def __str__(self):
-		return repr(self.value)
